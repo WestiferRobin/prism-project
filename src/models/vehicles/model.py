b5e447a596ec1e48b1a8e4fe630035a4ab441eb1
Wes <w.webb0919@gmail.com>
2025-02-05 04:43:40 -0800

    not complete but working fotf as matter of time to produce prism-drones and nexus-engines for my admin-legion

--- src/models/vehicles/model.py
+++ src/models/vehicles/model.py
@@ -1,6 +1,34 @@
+from src.models.drones.model import PrismAgent, PrismDrone
 
-class Ship:
-    def __init__(self, owner_id, copilot, engines):
-        self.owner_id = owner_id
-        self.copilot = copilot
-        self.engines = engines
+
+class Engine:
+    def __init__(self, ship_id):
+        self.id = ship_id
+
+class NexusEngine:
+    def __init__(self, ship_id):
+        super().__init__(ship_id)
+
+class OrbDrone:
+    def __init__(self, pilot: PrismAgent):
+        self.engines = NexusEngine()
+        self.pilot = pilot
+
+class SpaceSpeeder(OrbDrone):
+    def __init__(self, pilot: PrismDrone, co_pilot: PrismAgent):
+        super().__init__(pilot.agent(), co_pilot)
+        self.engines = (
+            NexusEngine(),
+            NexusEngine(),
+            NexusEngine(),
+            NexusEngine()
+        )
+        self.position = {
+
+        }
+
+class SpaceShuttle(Ship):
+    def __init__(self, pilot: PrismDrone, co_pilot: PrismDrone, cargo: list = None):
+        super(pilot.id, co_pilot.id)
+
+class SpaceShip()
--- src/simulations/engine_simulations.py
+++ src/simulations/engine_simulations.py
@@ -12,8 +12,7 @@ def run_post_ignition(nexus_engine: NexusEngine):
     nexus_engine.stop()
     print("post_ignition: Inconclusive")
 
-def run_engine_simulation():
-    nexus_engine = NexusEngine()
-    run_pre_ignition(nexus_engine)
-    run_engine(nexus_engine)
-    run_post_ignition(nexus_engine)
+def run_engine_simulation(engine: NexusEngine):
+    run_pre_ignition(engine)
+    run_engine(engine)
+    run_post_ignition(engine)
