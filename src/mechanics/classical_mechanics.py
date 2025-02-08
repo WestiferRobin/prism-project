9808f1655cbd40a2d9c2ed2155a96a7d4c48439a
Wesley  Webb <w.webb0919@gmail.com>
2025-02-08 04:48:39 -0800

    graph is coming together although position formula isnt working and needs to refactor

--- src/mechanics/classical_mechanics.py
+++ src/mechanics/classical_mechanics.py
@@ -1,31 +1,40 @@
 import numpy as np
 
-from src.graph.model import Graph, plot_function, plot_area
+from src.graph.model import Graph
 
+class PositionEquation:
+    def __init__(self, acceleration, initial_velocity, initial_position):
+        self.acceleration = acceleration
+        self.velocity = lambda t: initial_velocity + t * self.acceleration(t)
+        self.position = lambda t: initial_position + t * initial_velocity + 0.5 * self.acceleration(t) * t ** 2
 
-def acceleration(time):
-    return time * 0 + 1
+class PositionGraph:
+    def __init__(self, position_variable: str="x", time_variable: str="t"):
+        self.graph = Graph("Position-Time Graph", y_axis=f"Position({position_variable})", x_axis=f"Time({time_variable})")
 
+    def plot_interval(self, t_start, t_end, equation: PositionEquation):
+        times = np.linspace(t_start, t_end)
+        self.graph.plot_function(lambda t: equation.position(t), times, "position(t)", "blue")
+        self.graph.plot_function(lambda t: equation.velocity(t), times, "velocity(t)", "green")
+        self.graph.plot_function(lambda t: equation.acceleration(t), times, "acceleration(t)", "red")
 
-def velocity(time, initial_velocity):
-    final_velocity = initial_velocity
-    final_velocity += acceleration(time) * time
-    return final_velocity
+    def plot_area(self, t_start, t_end, equation: PositionEquation):
+        times = np.linspace(t_start, t_end)
+        self.graph.plot_area(lambda t: equation.position(t), times, "position(t)", "blue")
+        self.graph.plot_area(lambda t: equation.velocity(t - t_end + t_start), times, "velocity(t)", "green")
+        self.graph.plot_area(lambda t: equation.acceleration(t - t_end + t_start), times, "acceleration(t)", "red")
 
-
-def position(time, initial_position, initial_velocity):
-    final_position = initial_position
-    final_position += initial_velocity * time
-    final_position += (1/2) * acceleration(time) * time ** 2
-    return final_position
+    def show(self):
+        self.graph.plot()
 
 
 if __name__ == "__main__":
-    plot_graph = Graph("Physics", x_axis="t", y_axis="x")
-    times = np.linspace(0, 1)
-    position_0 = 0
-    velocity_0 = 0
-    plot_area(lambda t : position(t, position_0, velocity_0), times, "position(t)", "blue")
-    plot_function(lambda t : velocity(t, velocity_0), times, "velocity(t)", "green")
-    plot_function(lambda t : acceleration(t), times, "acceleration(t)", "red")
-    plot_graph.plot()
+    graph = PositionGraph()
+
+    asdf = PositionEquation(lambda t : t*0 + 1, 0, 0)
+    graph.plot_interval(0, 1, asdf)
+
+    fdsa = PositionEquation(lambda t : t*0 - 1, asdf.velocity(1), asdf.position(1))
+    graph.plot_area(1, 2, fdsa)
+
+    graph.show()
